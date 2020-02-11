c21e6ca1e682b4c4f5ff3f060750567489b667ae
mreguant <mar.reguant@gmail.com>
2020-02-10 17:39:48 -0800

    double lasso with 12 cores and fewer models

--- Build/R03_predictions_hourly_double_lasso.R
+++ Build/R03_predictions_hourly_double_lasso.R
@@ -16,7 +16,7 @@ setwd("T:/Projects/Schools/Data")
 set.seed(12345)
 
 #### SET UP PARALLEL STUFF
-myCores <- 6
+myCores <- 12
 cl <- makeCluster(myCores)
 clusterEvalQ(cl, library(stats))
 clusterEvalQ(cl, library("crayon", lib="T:/Projects/Schools/RLibraries"))
@@ -96,29 +96,29 @@ estimateModel <- function(i) {
     
     
     # LASSO - dataset
-    Xall = sparse.model.matrix(formula(paste("~ splineTemp1 + splineTemp2 + splineTemp3 + splineTemp4 + splineTemp5",
-                                             "+ splineTemp6 + splineTemp7 + splineTemp8 + splineTemp9 + temp_min + temp_max",
-                                             "+ factor(month) + factor(month)*temp_min + factor(month)*temp_max",
-                                             "+ factor(holiday) + factor(holiday)*temp_min + factor(holiday)*temp_max",
-                                             "+ factor(weekday) + factor(weekday)*temp_min + factor(weekday)*temp_max",
-                                             "+ factor(weekday)*factor(holiday) + factor(month)*factor(holiday) + factor(month)*factor(weekday)",
-                                             "+ factor(weekday)*splineTemp1 + factor(weekday)*splineTemp2", 
-                                             "+ factor(weekday)*splineTemp3 + factor(weekday)*splineTemp4", 
-                                             "+ factor(weekday)*splineTemp5 + factor(weekday)*splineTemp6",                                              
-                                             "+ factor(weekday)*splineTemp7 + factor(weekday)*splineTemp8 + factor(weekday)*splineTemp9",   
-                                             "+ factor(holiday)*splineTemp1 + factor(holiday)*splineTemp2", 
-                                             "+ factor(holiday)*splineTemp3 + factor(holiday)*splineTemp4", 
-                                             "+ factor(holiday)*splineTemp5 + factor(holiday)*splineTemp6",                                              
-                                             "+ factor(holiday)*splineTemp7 + factor(holiday)*splineTemp8 + factor(holiday)*splineTemp9",         
-                                             "+ factor(month)*splineTemp1 + factor(month)*splineTemp2", 
-                                             "+ factor(month)*splineTemp3 + factor(month)*splineTemp4", 
-                                             "+ factor(month)*splineTemp5 + factor(month)*splineTemp6",                                              
-                                             "+ factor(month)*splineTemp7 + factor(month)*splineTemp8 + factor(month)*splineTemp9",                                                                                 
-                                             "+ factor(weekday)*factor(month)*splineTemp1 + factor(weekday)*factor(month)*splineTemp2",
-                                             "+ factor(weekday)*factor(month)*splineTemp3 + factor(weekday)*factor(month)*splineTemp4",
-                                             "+ factor(weekday)*factor(month)*splineTemp5 + factor(weekday)*factor(month)*splineTemp6",
-                                             "+ factor(weekday)*factor(month)*splineTemp7 + factor(weekday)*factor(month)*splineTemp8",
-                                             "+ factor(weekday)*factor(month)*splineTemp9")),data=dataset)
+    # Xall = sparse.model.matrix(formula(paste("~ splineTemp1 + splineTemp2 + splineTemp3 + splineTemp4 + splineTemp5",
+    #                                          "+ splineTemp6 + splineTemp7 + splineTemp8 + splineTemp9 + temp_min + temp_max",
+    #                                          "+ factor(month) + factor(month)*temp_min + factor(month)*temp_max",
+    #                                          "+ factor(holiday) + factor(holiday)*temp_min + factor(holiday)*temp_max",
+    #                                          "+ factor(weekday) + factor(weekday)*temp_min + factor(weekday)*temp_max",
+    #                                          "+ factor(weekday)*factor(holiday) + factor(month)*factor(holiday) + factor(month)*factor(weekday)",
+    #                                          "+ factor(weekday)*splineTemp1 + factor(weekday)*splineTemp2", 
+    #                                          "+ factor(weekday)*splineTemp3 + factor(weekday)*splineTemp4", 
+    #                                          "+ factor(weekday)*splineTemp5 + factor(weekday)*splineTemp6",                                              
+    #                                          "+ factor(weekday)*splineTemp7 + factor(weekday)*splineTemp8 + factor(weekday)*splineTemp9",   
+    #                                          "+ factor(holiday)*splineTemp1 + factor(holiday)*splineTemp2", 
+    #                                          "+ factor(holiday)*splineTemp3 + factor(holiday)*splineTemp4", 
+    #                                          "+ factor(holiday)*splineTemp5 + factor(holiday)*splineTemp6",                                              
+    #                                          "+ factor(holiday)*splineTemp7 + factor(holiday)*splineTemp8 + factor(holiday)*splineTemp9",         
+    #                                          "+ factor(month)*splineTemp1 + factor(month)*splineTemp2", 
+    #                                          "+ factor(month)*splineTemp3 + factor(month)*splineTemp4", 
+    #                                          "+ factor(month)*splineTemp5 + factor(month)*splineTemp6",                                              
+    #                                          "+ factor(month)*splineTemp7 + factor(month)*splineTemp8 + factor(month)*splineTemp9",                                                                                 
+    #                                          "+ factor(weekday)*factor(month)*splineTemp1 + factor(weekday)*factor(month)*splineTemp2",
+    #                                          "+ factor(weekday)*factor(month)*splineTemp3 + factor(weekday)*factor(month)*splineTemp4",
+    #                                          "+ factor(weekday)*factor(month)*splineTemp5 + factor(weekday)*factor(month)*splineTemp6",
+    #                                          "+ factor(weekday)*factor(month)*splineTemp7 + factor(weekday)*factor(month)*splineTemp8",
+    #                                          "+ factor(weekday)*factor(month)*splineTemp9")),data=dataset)
     
     # LASSO - dataset
     Xallcontrols = sparse.model.matrix(formula(paste("~ splineTemp1 + splineTemp2 + splineTemp3 + splineTemp4 + splineTemp5",
@@ -155,16 +155,16 @@ estimateModel <- function(i) {
     
     Y = dataset$qkw_hour
     
-    prediction.model = cv.glmnet(Xall,Y,foldid=dataset$foldid,family="gaussian",alpha=1)
-    dataset$prediction_dl1 <- c(predict(prediction.model,Xall,type="link", s = "lambda.min"))
-    dataset$prediction_dl2 <- c(predict(prediction.model,Xall,type="link", s = "lambda.1se"))
+    #prediction.model = cv.glmnet(Xall,Y,foldid=dataset$foldid,family="gaussian",alpha=1)
+    #dataset$prediction_dl1 <- c(predict(prediction.model,Xall,type="link", s = "lambda.min"))
+    #dataset$prediction_dl2 <- c(predict(prediction.model,Xall,type="link", s = "lambda.1se"))
     
     prediction.modelc = cv.glmnet(Xallcontrols,Y,foldid=dataset$foldid,family="gaussian",alpha=1)
     dataset$prediction_dl3 <- c(predict(prediction.modelc,Xallcontrols,type="link", s = "lambda.min"))
     dataset$prediction_dl4 <- c(predict(prediction.modelc,Xallcontrols,type="link", s = "lambda.1se"))
     
     #reduce size
-    dataset <- dataset[,c("date", "qkw_hour", "prediction_dl1",  "prediction_dl2",  "prediction_dl3", "prediction_dl4")]
+    dataset <- dataset[,c("date", "qkw_hour", "prediction_dl3", "prediction_dl4")]
     
     write.csv(dataset,file=paste0("Intermediate/School specific/double lasso/school_data_",i,"_prediction_dl_block",b,".csv"))
     
